926d9a6827746a7753cfd3b3f818c0137a4cc82c
Tony Jenkins <A.M.Jenkins@leedsbeckett.ac.uk>
2022-10-11 15:58:11 +0100

    Tweak final marks program

--- labs/tony/tuesday-cs4d/2022-10-11/marks_13.py
+++ labs/tony/tuesday-cs4d/2022-10-11/marks_13.py
@@ -21,11 +21,11 @@ if __name__ == '__main__':
             print('Error: Value is not an integer.')
 
     try:
-        print(f'Highest Mark: {max(marks)}')
-        print(f'Lowest Mark:  {min(marks)}')
-        print(f'Average Mark: {avg(marks):.2f}')
-        print(f'Median Mark:  {median(marks):.2f}')
-        print(f'Std Devn:     {stdev(marks):.2f}')
+        print(f'Highest Mark:   {max(marks)}')
+        print(f'Lowest Mark:    {min(marks)}')
+        print(f'Average Mark:   {avg(marks):.2f}')
+        print(f'Median Mark:    {median(marks):.2f}')
+        print(f'Standard Devn:  {stdev(marks):.2f}')
 
     except ValueError:
         print('No marks entered. Nothing to do.')
