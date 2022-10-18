48a785d427aa0a253dd1bfb372ddd4d268eb56fa
Tony Jenkins <A.M.Jenkins@leedsbeckett.ac.uk>
2022-10-18 12:38:00 +0100

    Fix borked solution in week 3

--- portfolio/week03/hello_stranger.py
+++ portfolio/week03/hello_stranger.py
@@ -2,4 +2,4 @@
 
 if __name__ == '__main__':
     name = input('Hello, what is your name? ')
-    print(f'Hello, {"name" if name else "Stranger"}! It is good to meet you!')
+    print(f'Hello, {name if name else "Stranger"}! It is good to meet you!')
